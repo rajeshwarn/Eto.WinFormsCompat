41798952211d86a0d26db24a143c79c1027ec2bb
0xFireball <0xFireball@outlook.com>
2016-04-29 21:07:28 -0700

    add project source files

---
+++ Eto.WinFormsCompat/Eto.WinFormsCompat/EtoWinFormsCompatStubOnlyAttribute.cs
@@ -0,0 +1,16 @@
+﻿/*
+ */
+using System;
+
+namespace System
+{
+	/// <summary>
+	/// An attribute signifying that the specified member is only a stub to maintain compatibility with the WinForms API.
+	/// </summary>
+	public class EtoWinFormsCompatStubOnlyAttribute : Attribute
+	{
+		public EtoWinFormsCompatStubOnlyAttribute()
+		{
+		}
+	}
+}
