0d49845a0e0ce736fe87187704397a58523b6e4b
0xFireball <0xFireball@outlook.com>
2016-04-29 21:09:49 -0700

    add drawing classes

---
+++ Eto.WinFormsCompat/Eto.WinFormsCompat/Drawing/SizeF.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Drawing
+{
+    public struct SizeF
+    {
+        public float Height;
+        public float Width;
+
+        public SizeF(float width, float height)
+        {
+            Height = height;
+            Width = width;
+        }
+    }
+}
---
+++ Eto.WinFormsCompat/Eto.WinFormsCompat/Windows/Forms/Form.cs
@@ -0,0 +1,13 @@
+﻿/*
+ */
+using System;
+
+namespace System.Windows.Forms
+{
+	public class Form
+	{
+		public Form()
+		{
+		}
+	}
+}
