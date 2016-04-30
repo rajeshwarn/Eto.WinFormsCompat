86cabf76ebf6bf23f9992fb54c2d2907737e3c15
0xFireball <0xFireball@outlook.com>
2016-04-30 12:15:31 -0700

    update designer project

--- Eto.WinFormsCompat/WinFormsCompatDemo.Designer/MainForm.Designer.cs
+++ Eto.WinFormsCompat/WinFormsCompatDemo.Designer/MainForm.Designer.cs
@@ -1,58 +1,46 @@
-﻿/*
- */
-namespace WinFormsCompatDemo.Designer
+﻿namespace WinFormsCompatDemo.Designer
 {
-	partial class MainForm
-	{
-        #region Private Fields
-
+    partial class MainForm
+    {
         /// <summary>
-        /// Designer variable used to keep track of non-visual components.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        #endregion Private Fields
-
-        #region Protected Methods
-
         /// <summary>
-        /// Disposes resources used by the form.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
-		{
-			if (disposing) {
-				if (components != null) {
-					components.Dispose();
-				}
-			}
-			base.Dispose(disposing);
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        #endregion Protected Methods
-
-        #region Private Methods
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// This method is required for Windows Forms designer support.
-        /// Do not change the method contents inside the source code editor. The Forms designer might
-        /// not be able to load this method if it was changed manually.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
-		{
-			this.SuspendLayout();
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(585, 383);
-			this.Name = "MainForm";
-			this.Text = "WinFormsCompatDemo.Designer";
-			this.ResumeLayout(false);
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 380);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
 
         }
 
-        #endregion Private Methods
+        #endregion
     }
 }
--- Eto.WinFormsCompat/WinFormsCompatDemo.Designer/MainForm.cs
+++ Eto.WinFormsCompat/WinFormsCompatDemo.Designer/MainForm.cs
@@ -1,27 +1,12 @@
-﻿/*
- */
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace WinFormsCompatDemo.Designer
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
-		}
-	}
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
 }
