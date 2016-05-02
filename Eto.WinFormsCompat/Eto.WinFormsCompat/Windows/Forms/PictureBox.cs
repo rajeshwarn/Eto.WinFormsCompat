51733ed08b81fad93d700ba38ac1961046f731ad
0xFireball <0xFireball@outlook.com>
2016-05-01 20:59:45 -0700

    add stub picturebox implementation

---
+++ Eto.WinFormsCompat/Eto.WinFormsCompat/Windows/Forms/PictureBox.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Windows.Forms
+{
+    public class PictureBox : Control
+    {
+        #region Public Constructors
+
+        public PictureBox()
+        {
+            EtoImageView = new Eto.Forms.ImageView();
+            EtoControl = EtoImageView;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public Eto.Forms.ImageView EtoImageView { get; }
+
+        #endregion Public Properties
+    }
+}
